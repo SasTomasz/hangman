8bf70477f4f12e8c3367094132d3a927733b09ff
Tomasz Sas <sastomasz@gmail.com>
2022-02-10 12:57:05 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+#TODO 01 Make small list of words
+#TODO 02 When game starts choose one word and show to the user how many letters it has
+#TODO 03 Give users three chances to ask wrong letter
+#TODO 04 When user guess letter all that letter shows out
+
+words = ['Elephant', 'Apple']
