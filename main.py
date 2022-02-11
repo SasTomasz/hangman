db11dc9cc31ec43dabf485dcc264de73e743137f
Tomasz Sas <sastomasz@gmail.com>
2022-02-11 10:15:10 +0100

    Draw word and show how many letters it has

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-#TODO 01 Make small list of words
-#TODO 02 When game starts choose one word and show to the user how many letters it has
 #TODO 03 Give users three chances to ask wrong letter
 #TODO 04 When user guess letter all that letter shows out
 
-words = ['Elephant', 'Apple']
+import random
+
+def draw_word(word): 
+    guessing_word = ['_'] * len(word)
+    return guessing_word
+
+def guessing_the_word(word):
+    print('   '.join(word))
+    pass
+
+words = ['Elephant', 'Apple', 'Car', 'Destination', 'Minute']
+guessing_word = draw_word(random.choice(words))
+
+guessing_the_word(guessing_word)
+
+
+
