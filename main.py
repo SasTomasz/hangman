b2809db65a6cf33c038101f8b63f0329d6b601da
Tomasz Sas <sastomasz@gmail.com>
2022-02-14 11:58:50 +0100

    User can guessing the word infinitely

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
 #TODO 03 Give users three chances to ask wrong letter
-#TODO 04 When user guess letter all that letter shows out
 
 import random
 
-def draw_word(word): 
-    guessing_word = ['_'] * len(word)
-    return guessing_word
+words = ['ELEPHANT', 'APPLE', 'CAR', 'DESTINATION', 'MINUTE']
+word = ''
+guessed_word = ''
 
-def guessing_the_word(word):
-    print('   '.join(word))
+def draw_word(word): 
+    guessed_word = ['_'] * len(word)
+    return guessed_word, word
+
+""" 
+word is word guessing by user
+letter is letter give by user
+returns new representation of guessing word with exposed letters 
+"""
+def check_letter(guessed_word, word, letter):
+    for x in range(len(word)):
+       if word[x] == letter:
+           guessed_word[x] = letter 
+
+    return guessed_word
+
+def guessing_the_word(guessed_word, word):
+    # wrong_guess = 0
+    letter = ''
+    win = False
+
+    while win == False:
+        print('   '.join(guessed_word))
+        letter = input("Guess the letter in the word: ").upper()
+        guessed_word = check_letter(guessed_word, word, letter)
+        if guessed_word.count('_') == 0:
+            win = True
+            print("Congratulations, You guess word: ", word)
     pass
 
-words = ['Elephant', 'Apple', 'Car', 'Destination', 'Minute']
-guessing_word = draw_word(random.choice(words))
 
-guessing_the_word(guessing_word)
+guessed_word, word = draw_word(random.choice(words))
+guessing_the_word(guessed_word, word)
 
 
 
